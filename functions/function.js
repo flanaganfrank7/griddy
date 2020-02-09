82bd487480548d57d0520ed1d92918544b541bab
frank <frankflanagan17@gmail.com>
2020-02-09 11:28:44 -0500

    height

--- functions/function.js
+++ functions/function.js
@@ -1,4 +1,8 @@
-// bodyHeight = docu
+var body = document.body,
+    html = document.documentElement;
+
+var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+alert(height)
 
 gridSize = document.currentScript.getAttribute('size');
 gridColor = document.currentScript.getAttribute('color');
